78a4032bcf64f6597a197714adee862e4558047e
Zack Ames <zames6584@gmail.com>
2024-01-09 21:12:27 -0500

    fix sdf

--- client/src/shaders/basic.tsx
+++ client/src/shaders/basic.tsx
@@ -1,5 +1,5 @@
 import { OrbitControls, PerspectiveCamera, Box } from '@react-three/drei'
-import { MeshBasicNodeMaterial, ShaderNodeObject, Node, vec3, uv, length} from 'three/examples/jsm/nodes/Nodes.js'
+import { MeshBasicNodeMaterial, ShaderNodeObject,mul,cond, Node, sub, vec2, vec3, positionLocal, uv, length, greaterThan} from 'three/examples/jsm/nodes/Nodes.js'
 import { WebGPUCanvas } from '../WebGPUCanvas.tsx'
 import { FC } from 'react'
 
@@ -13,13 +13,19 @@ const material = new MeshBasicNodeMaterial()
 
 const Basic: FC<BasicProps> = ({color}) => {
 
-    
-    material.colorNode = color
+    const sd = (p:any, r:any) => {
+        return sub(length(p), r);
+    }
+
+    let uv0 = positionLocal;
+    let d = sd(uv0.xy, .5);
+
+    material.colorNode = cond(d.greaterThan(0), mul(d , color), vec3(0,0,1));
 
     return (
         <WebGPUCanvas>
 
-                <mesh>
+                <mesh >
                     <planeGeometry args={[3,3]}/>
                     <primitive attach="material" object={material} />
                 </mesh>
